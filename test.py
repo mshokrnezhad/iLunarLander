65247e54ffdbc148391a3fea265225798d89a070
Masoud <m.shokrnezhad@gmail.com>
2024-05-17 15:40:31 +0300

    a test file is added to illustrate how the lunar lander works with random action selection strategy.

---
+++ test.py
@@ -0,0 +1,16 @@
+import gym
+
+if __name__ == "__main__":
+    env = gym.make("LunarLander-v2", render_mode="human")
+
+    state = env.reset()
+    done = False
+    score = 0
+    while not done:
+        action = env.action_space.sample()
+        state_, reward, done, info, _ = env.step(action)
+        score += reward
+        env.render()
+        print(reward, score)
+        
+    
