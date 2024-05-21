cb68e913e790f28d8e06122da25db70fb8d24932
Masoud Shokrnezhad <m.shokrnezhad@gmail.com>
2024-05-22 00:27:29 +0300

    the gif creator has been commented in main.py.

--- REINFORCE/main.py
+++ REINFORCE/main.py
@@ -24,8 +24,8 @@ if __name__ == "__main__":
     final_landing_gif_file = str(current_dir) + "/plots/" + file_name + "_final.gif"
     
     scores = []
-    first_frames = []
-    final_frames = []
+    #first_frames = []
+    #final_frames = []
     
     for t in range(num_games):
         done = False
@@ -35,10 +35,10 @@ if __name__ == "__main__":
             action = agent.act(state)
             state_, reward, done, info, _ = env.step(action)
             score += reward 
-            if(t == 0):
-                first_frames.append(env.render())
-            if(t == num_games - 1):
-                final_frames.append(env.render())
+            #if(t == 0):
+            #    first_frames.append(env.render())
+            #if(t == num_games - 1):
+            #    final_frames.append(env.render())
             agent.store_reward(reward)
             state = state_
         agent.learn()
@@ -47,6 +47,6 @@ if __name__ == "__main__":
         avg_score_ = np.mean(scores[-100:])
         print("step", t, "- score %.2f" %score, "- avg_score %.2f" %avg_score_)
     
-    save_frames_as_gif(first_frames, first_landing_gif_file)
-    save_frames_as_gif(final_frames, final_landing_gif_file)
+    #save_frames_as_gif(first_frames, first_landing_gif_file)
+    #save_frames_as_gif(final_frames, final_landing_gif_file)
     plot_learning_curve(scores, scores_plot_file)            
