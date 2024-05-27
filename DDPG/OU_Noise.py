4b89092823f141d255f2df3e5b63aa66b2dfdbbb
Masoud <m.shokrnezhad@gmail.com>
2024-05-27 17:15:32 +0300

    OU_Noise has been added.

---
+++ DDPG/OU_Noise.py
@@ -0,0 +1,23 @@
+#       In this file, we implement a class for noise gneration based on the Ornstein–Uhlenbeck process.
+
+import numpy as np
+
+class OU_Noise:
+    def __init__(self, mu, sigma = 0.5, theta = 0.2, dt = 1e-2, x0 = None):
+        self.mu = mu
+        self.sigma = sigma
+        self.theta = theta
+        self.dt = dt
+        self.x0 = x0
+        
+        self.reset()
+
+    def __call__(self):
+        dif_ = self.mu - self.x_perv
+        rnd_ = np.random.normal(size = self.mu.shape)
+        x = self.x_prev + self.theta * self.dt * dif_ + self.sigma + np.sqrt(self.dt) + rnd_
+        
+        return x
+    
+    def reset(self):
+        self.x_prev = self.x0 if self.x0 is not None else np.zeros_like(self.mu)    
