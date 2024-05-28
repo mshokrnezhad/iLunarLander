d10f3cc09a69479bd8c87adc9d4d2a0d45c88e6c
Masoud <m.shokrnezhad@gmail.com>
2024-05-29 01:28:26 +0300

    folders have been created.

--- DDPG/DDPG_Agent.py
+++ DDPG/DDPG_Agent.py
@@ -62,7 +62,7 @@ class DDPG_Agent():
         self.online_ADN.eval() #5
         state = T.tensor([state], dtype=T.float).to(self.online_ADN.device) #6
         mu = self.online_ADN.forward(state).to(self.online_ADN.device) #7
-        noise = T.tensor([self.noise()], dtype=T.float).to(self.online_ADN.device) #8
+        noise = T.tensor(self.noise(), dtype=T.float).to(self.online_ADN.device) #8
         mu_ = mu + noise 
         self.online_ADN.train() #9
         
